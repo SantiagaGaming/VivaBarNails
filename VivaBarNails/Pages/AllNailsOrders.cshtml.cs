2ae2f4f4fd7a4824f048482db4a5d34ddcd83146
SantiagaGaming <ganychev92@gmail.com>
2022-08-10 18:30:12 +0300

    Added Nailorders Page

---
+++ VivaBarNails/Pages/AllNailsOrders.cshtml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using VivaBarNails.Data;
+using VivaBarNails.Models;
+
+namespace VivaBarNails.Pages
+{
+    public class AllNailsOrders : PageModel
+    {
+        private ApplicationDbContext _context;
+        public List<NailOrder> Orders;
+
+        public AllNailsOrders(ApplicationDbContext context)
+        {
+           _context = context;
+        }
+
+        public void OnGet()
+        {
+            Orders = _context.NailOrders.ToList();
+        }
+    }
+}
