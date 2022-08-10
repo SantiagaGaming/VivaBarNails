6d547b49fdcecff97ceac766e8bcb8c47c8c7271
SantiagaGaming <ganychev92@gmail.com>
2022-08-10 16:31:13 +0300

    Phone Validation added

--- VivaBarNails/Models/Client.cs
+++ VivaBarNails/Models/Client.cs
@@ -1,10 +1,19 @@
-﻿namespace VivaBarNails.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VivaBarNails.Models
 {
     public class Client
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Укажите имя для записи.")]
         public string? ClientName { get; set; }
+
+        [Required(ErrorMessage ="Укажите номер телефона.")]
+        [StringLength(10, MinimumLength = 10, ErrorMessage = "Номер телефона должен состоять из 10 цифр.(9215556565)")]
+        [RegularExpression(@"^[1-9]+[0-9]*$", ErrorMessage = "Номер телефона может содержать только цифры!")]
+        [Phone]
         public int PhoneNumber { get; set; }
+        [Required(ErrorMessage = "Выберите свободную дату для записи.")]
         public DateTime VisitDate { get; set; }
         public NailMaster? NailMaster { get; set; }
 
