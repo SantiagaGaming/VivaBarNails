0ce57c5c8b03eff031d5255618719d932c7123fb
SantiagaGaming <ganychev92@gmail.com>
2022-08-18 19:17:33 +0300

    Add Constants

---
+++ VivaBarNails/Authorization/Constants.cs
@@ -0,0 +1,8 @@
+﻿namespace VivaBarNails.Authorization
+{
+    public class Constants
+    {
+        public static readonly string ManagerRole = "ManagerRole";
+        public static readonly string AdminRole = "AdminRole";
+    }
+}
